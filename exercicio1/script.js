44ee10b8c5b4d323100002a55cc9199643887cb4
mauricio oliveira <snarfalternativa@hotmail.com>
2022-11-23 16:13:54 -0300

    terminando exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+/*a) Declare uma função que imprima `Olá, [SEU NOME]!`. Chame esta função.
+
+b) Declare uma função que receba um número como parâmetro imprima no console a tabuada do dele. Chame esta função várias vezes com números diferentes.
+
+c) Comente as funções criadas acima, e reescreva-as utilizando expressões de função, ou __arrow functions__ */
+
+const tabuada = (num) => {
+    return[num*0,num*1,num*2,num*3,num*4,num*5,num*6,num*7,num*8,num*9,num*10]
+}
+console.log(tabuada(2))
